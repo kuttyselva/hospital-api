21f1dfd9ab805a1d3b446ee4c680bdd93a26c5b9
kuttyselva <33373961+kuttyselva@users.noreply.github.com>
2020-02-03 00:27:09 +0530

    hehe

--- src/test/java/global/coda/hms/controller/DoctorControlllerTest.java
+++ src/test/java/global/coda/hms/controller/DoctorControlllerTest.java
@@ -5,15 +5,12 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import java.nio.charset.Charset;
-import global.coda.hms.model.DoctorRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
+
 import org.junit.jupiter.api.*;
 @SpringBootTest
 @AutoConfigureMockMvc
