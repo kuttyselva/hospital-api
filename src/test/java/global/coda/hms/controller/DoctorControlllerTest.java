d4fb33144001c214b7d56767cb37c88ed0536985
kuttyselva <33373961+kuttyselva@users.noreply.github.com>
2020-02-03 22:19:25 +0530

    jenkins auto build

--- src/test/java/global/coda/hms/controller/DoctorControlllerTest.java
+++ src/test/java/global/coda/hms/controller/DoctorControlllerTest.java
@@ -1,7 +1,6 @@
 package global.coda.hms.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import java.nio.charset.Charset;
 
